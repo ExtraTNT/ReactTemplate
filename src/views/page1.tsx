54e3c42e6fdb10e517b25eb893d23297e828e394
ExtraTNT <sven.hugi@swisstxt.ch>
2021-10-26 21:03:54 +0200

    make shit better

--- src/views/page1.tsx
+++ src/views/page1.tsx
@@ -47,7 +47,7 @@ export const Page1 = (props) => {
                         <p className={classes.darkPrimaryColor}>{post.body}</p>
                     </div> 
                 })}
-                {!data && <h1 className={classes.bold} >Loading...</h1>}
+                {(!data && !error) && <h1 className={classes.bold}>Loading...</h1>}
             </div>
             
             <PopupError title={errorPopupTitle} text={errorPopupMessage} onClick={handleClick} active={errorPopupActive}/>  
